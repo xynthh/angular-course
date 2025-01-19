fa3696b743d9a773fdea07157b2dd5a43d781063
sandersm <sandersm@student.21-school.ru>
2025-01-19 14:16:26 +0300

    Compontnt @input decorator 2

--- src/app/components/course-card/course-card.component.ts
+++ src/app/components/course-card/course-card.component.ts
@@ -1,12 +1,24 @@
-import {Component, Input} from '@angular/core';
-import {CourseModel} from './models/course.model';
+import { Component, Input, OnInit } from '@angular/core';
+import { CourseModel } from './models/course.model';
 
 @Component({
   selector: 'app-course-card',
   imports: [],
   templateUrl: './course-card.component.html',
-  styleUrl: './course-card.component.css'
+  styleUrl: './course-card.component.css',
 })
-export class CourseCardComponent {
+export class CourseCardComponent implements OnInit {
   @Input() course!: CourseModel;
+
+  constructor() {
+    console.log('Course card component created');
+  }
+
+  ngOnInit(): void {
+    console.log('Course card component initialized');
+  }
+
+  onCourseViewed() {
+    console.log('Course viewed: ', this.course.description);
+  }
 }
