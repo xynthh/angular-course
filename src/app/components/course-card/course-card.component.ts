66738fdbc7affdb1ff443b008168a9b0e3ac2dc6
sandersm <sandersm@student.21-school.ru>
2025-01-25 14:59:05 +0300

    ngSwitch Core Directive

--- src/app/components/course-card/course-card.component.ts
+++ src/app/components/course-card/course-card.component.ts
@@ -1,14 +1,16 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CourseModel } from './models/course.model';
-import { NgClass } from '@angular/common';
+import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 
 @Component({
   selector: 'app-course-card',
-  imports: [
-    NgClass,
-  ],
   templateUrl: './course-card.component.html',
   styleUrl: './course-card.component.css',
+  imports: [
+    NgSwitch,
+    NgSwitchCase,
+    NgSwitchDefault,
+  ],
 })
 export class CourseCardComponent {
   @Input({
