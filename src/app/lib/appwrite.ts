21a22efd1c6c08aa552731cc056ae09cd58ec713
xynthh <wivvz@yandex.ru>
2025-04-10 23:12:07 +0300

    del appwrite.ts

--- src/app/lib/appwrite.ts
+++
@@ -1,7 +0,0 @@
-import { Account, Client } from 'appwrite';
-
-export const client = new Client();
-client.setProject('678f9ca40018f19534c5');
-
-export const account = new Account(client);
-export { ID } from 'appwrite';
