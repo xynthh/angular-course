ef41edb511b6864305a35b45eae95963505f28d0
xynthh <wivvz@yandex.ru>
2025-04-11 12:08:43 +0300

    @defer custom triggers
    ON - prebuild trigger
    WHEN - custom trigger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
-import { CourseCardComponent } from './components/course-card/course-card.component';
-import { CourseModel } from './components/course-card/models/course.model';
-import { COURSES } from './db-data';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {CourseCardComponent} from './components/course-card/course-card.component';
+import {CourseModel} from './components/course-card/models/course.model';
+import {COURSES} from './db-data';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -18,7 +18,18 @@ export class AppComponent {
   price = 9.99;
   rate = 0.67;
 
+  performPrefetch = false;
+  display = false;
+
   onCourseSelected(course: CourseModel) {
     console.log('App component - click event buddled...', course);
+  }
+
+  onPrefetch() {
+    this.performPrefetch = true;
+  }
+
+  onDisplay() {
+    this.display = true;
   }
 }
